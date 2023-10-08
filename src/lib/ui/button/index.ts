cf91d1aa5d2b7739ede0fb6323397a60b385c47c
Glitch <glitch.txs@gmail.com>
2023-10-08 16:26:28 -0300

    make button customizable

--- src/lib/ui/button/index.ts
+++ src/lib/ui/button/index.ts
@@ -1,4 +1,4 @@
-import {LitElement, html} from 'lit';
+import {LitElement, css, html} from 'lit';
 import {customElement} from 'lit/decorators.js';
 import { set } from '../../store';
 import { isMobile } from '../../utils/mobile';
@@ -6,13 +6,30 @@ import { isMobile } from '../../utils/mobile';
 @customElement('button-modal')
 export class ButtonModal extends LitElement {
 
-  open(){
+  static styles = css`
+  :host{
+    background-color: #FFFFFF;
+    border: 1px solid #222222;
+    color: #222222;
+    border-radius: 8px;
+    cursor: pointer;
+    font-family: inherit;
+    padding: 13px;
+  }
+  `
+
+  protected _open(){
     if(isMobile()) return // We open the URI on mobile
     set.open(true)
   }
 
+  constructor() {
+    super();
+    this.addEventListener('click', this._open);
+  }
+
   render() {
-    return html`<button @click="${this.open}" >Open Modal</button>`;
+    return html`<slot>Open Modal</slot>`;
   }
 }
 
