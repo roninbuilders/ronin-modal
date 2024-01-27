c4fc4f1904e7d198f7497817ebd776bb591c5303
Glitch <glitch.txs@gmail.com>
2024-01-27 20:05:35 -0300

    tsup config

--- packages/modal-wagmi-v2/tsup.config.ts
+++ packages/modal-wagmi-v2/tsup.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  splitting: true,
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   target: 'es2021',
   clean: true,
-  bundle: true,
   dts: true,
+  minify: true
 })
