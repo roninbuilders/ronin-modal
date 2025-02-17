1ccfa570608630719d5ee796f48904d6c6102a64
glitch-txs <glitch.txs@gmail.com>
2025-02-17 13:54:23 -0300

    fix svg issue, fix watch mode

--- examples/w3vm/react/src/App.tsx
+++ examples/w3vm/react/src/App.tsx
@@ -13,12 +13,13 @@ createRoninModal({
 	waypoint:{
 		clientId: "0e188f93-b419-4b0f-8df4-0f976da91ee6"
 	},
+	darkMode: true,
 	siweConfig: {
 		getNonce: generateNonce,
 		verifyMessage: ()=>true,
 		getSession: ()=> new Promise((resolve)=>resolve("session")),
 		signOut: ()=>{}
-	  }
+	  },
 })
 
 function App() {
